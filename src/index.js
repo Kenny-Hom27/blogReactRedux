b437521600668493afaaa89e95c51b64008835f9
github username <kenny.hom27@gmail.com>
2018-05-30 19:42:19 -0400

    added routes and simple redux

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import promise from 'redux-promise'
+
+import reducers from './reducers';
+import PostsIndex from './components/PostsIndex'
+import PostsNew from './components/PostsNew'
+
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/posts/new" component={PostsNew}/>
+        <Route exact path="/" component={PostsIndex}/>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
